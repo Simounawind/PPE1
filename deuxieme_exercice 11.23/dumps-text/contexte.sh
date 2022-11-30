3d7d3af1aee4e2c78fb3e86b8d0a06bd7e9fdcca
CUI <cuixiaohua1212@163.com>
2022-11-30 08:53:41 +0100

    11.30

--- deuxieme_exercice 11.23/dumps-text/contexte.sh
+++ deuxieme_exercice 11.23/dumps-text/contexte.sh
@@ -17,25 +17,34 @@ echo "
 				</thead>" >new.html
 num=1
 i=1
-while ((i <= 53))
+while ((i <= 52))
 do
-    dossier=urls-Xiaohua-$num.txt
+    dossier=fich-$i.txt
+    echo "$dossier"
     
-    cible_avant=$(egrep -o "[^，。？！]+种族歧视 |sort " $dossier)
-    while read before
+    egrep -o "[^，。？！]+种族歧视" $dossier | sort |
+    # echo "$cible_avant" > transf.txt
+    while read -r before
     do
-        echo "<tr><td>$before</td></tr>" >>new.html
-    done < $cible_avant
-    
-    cible=$(egrep -o "种族歧视 |sort " $dossier)
-    cible_apres=$(egrep -o "种族歧视.+[，。？！][^，。？！]+ |sort " $dossier)
-    echo "<tr><td>$cible_avant</td><td><strong>$cible</strong></td><td>$cible_apres</td></tr>" >>new.html
-    i=$((i+1));
-    num=$((num+1))
+        echo "<tr><td>$before</td></tr>" >> new.html
+    done
 
+
+    i=$((i+1));
 done
+
+
+
+# do
+#     echo "<tr><td>$before</td></tr>" >>new.html
+# done < $fichier_urls
+
+cible=$(egrep -o "种族歧视 |sort " $dossier)
+cible_apres=$(egrep -o "种族歧视.+[，。？！][^，。？！]+ |sort " $dossier)
+echo "<tr><td>$cible_avant</td><td><strong>$cible</strong></td><td>$cible_apres</td></tr>" >>new.html
+i=$((i+1));
+num=$((num+1))
+# done
 echo "</table></body></html>" >> new.html
 echo "Traitement terminé." >> new.html
 exit
-
-#Continuer à l'ex 4
