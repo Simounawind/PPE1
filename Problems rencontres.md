3a61251272d55fb5b49bc7cfe14a8ce1240cb7e2
Simounawind <1037674042@qq.com>
2023-01-09 14:59:41 +0100

    creation de site version 2

--- Problems rencontres.md
+++ Problems rencontres.md
@@ -1,4 +1,16 @@
-
+1. Au début du semestre, je ne connaissais presque rien au contrôle de version Git, j'ai donc rencontré beaucoup de problèmes de conflits de versions, j'ai donc tout appris sur Git sur le site vidéo bilibili, y compris les quatre partitions, les commandes courantes git rest -hard, reflog, merge hot-fix, etc. Depuis lors, j'ai été en mesure de gérer git.
+2. Je ne connais rien au shell et Bash, et je rencontre souvent des erreurs lors de l'exécution des programmes, ce qui réduit considérablement ma productivité. Il est donc nécessaire d'apprendre certaines des opérations de base et de comprendre son fonctionnement et ses règles. Après bien des difficultés, j'ai finalement donné un coup de jeune à mon terminal en utilisant `oh my zsh`, en installant git et le plugin de mise en évidence du code afin de pouvoir mieux visualiser les informations du projet.
+3. Qu'est-ce que sed ? Que signifient les symboles `$`, `&` etc... ? Cela reste inconnu.
+   Quels sont les paramètres du lynx ? Comment comprendre le code "lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL" ?
+   (toujours pas résolu)
+4. Selon le script posté par le professeur, pourquoi le tableau généré a-t-il de multiples cas "utf-8" ? (je prédis que le problème est dans la ligne de `curl` )
+   - problème résolu. Le professeur m'a dit que je devrais exclure des information du titre lorsque curl l'obtient.
+5. Question pour l'établissement de pages Web : Comment comprendre le rôle du class `container` ? Comment créer des effets CSS plus riches ? Comment comprendre la logique de l'utilisation de CSS ?
+6. Pourquoi les résultats de la recherche dans itrameur ne correspondent-ils pas à ce que je veux ? Certains mots à haute fréquence qui devraient apparaître sont maintenant disparus !
+   - Problème résolu. Pour le chinois, il faut **segmenter** les mots. J'ai donc utilisé **jieba** pour diviser les mots et j'ai obtenu le résultat idéal.
+7. Après avoir utilisé jieba pour segmenter les mots, certaines informations noyaux dans le fichier itrameur ne sont plus conservées, par exemple `<text>, <ch-page1>` deviennent maintenant ` < text > ,  < ch - page1 > `, même après avoir ajouté le dictionnaire utilisateur dict.txt, jieba les divisera toujours, pourquoi ?
+8. Pourquoi le code html et bash ne s'affiche-t-il pas correctement dans highlight.js ?
+   Problème résolu, en consultant d'autres personnes j'ai appris que pour les crochets pointus `<>` il faut remplacer par des caractères d'échappement. Par exemple : echo "&lt;/text&gt ;" &gt;&gt ; contexte.txt
 
 
 
--- projet_final/site_original/assets/js/main.js
+++ projet_final/site_original/assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: FlexStart - v1.12.0
-* Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function () {
-  "use strict";
 
   /**
    * Easy selector helper function
@@ -77,11 +69,6 @@
     })
   }
 
-
-
-  /**
-   * Hero carousel indicators
-   */
   let heroCarouselIndicators = select("#hero-carousel-indicators")
   let heroCarouselItems = select('#heroCarousel .carousel-item', true)
 
@@ -91,9 +78,6 @@
       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
   });
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -108,22 +92,6 @@
   }
 
   /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
-
-  /**
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function (e) {
@@ -131,16 +99,6 @@
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
-
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
 
   /**
    * Scrool with ofset on links with a class name .scrollto
@@ -170,141 +128,3 @@
       }
     }
   });
-
-  /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function (e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function (el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        aos_init();
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfokio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
-      }
-    }
-  });
-
-  /**
-   * Animation on scroll
-   */
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', () => {
-    aos_init();
-  });
-
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
-
-})();
